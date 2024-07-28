ecbfb3823b098d4db5d598a41a58d49af2776a0b
Young Jin <likeRudin@gmail.com>
2024-07-28 22:51:19 +0900

    [UI] add ArrowLink

---
+++ next-js-start/next-book-shelf/components/ui/ArrowLink/ArrowLink.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+import * as Styles from "./ArrowLink.module.css";
+
+interface IArrowLink {
+  text: string;
+  address: string;
+  isOuterLink: boolean;
+}
+
+const ArrowLink = ({ text, address, isOuterLink }: IArrowLink) =>
+  isOuterLink ? (
+    <a className={Styles.link} href={address}>
+      {`${text} ↪`}
+    </a>
+  ) : (
+    <Link className={Styles.link} href={address}>
+      {`${text} ↪`}
+    </Link>
+  );
+
+export default ArrowLink;
