faeb7461449c17b8e4f5b1c8de876e4d217f85a3
Young Jin <likeRudin@gmail.com>
2024-07-28 22:53:01 +0900

    [Feature] add Categories Component for home page

---
+++ next-js-start/next-book-shelf/components/Categories/Categories.tsx
@@ -0,0 +1,27 @@
+import { CATEGORIES_URL } from "@/app/constants";
+
+import { ArrowLink } from "../ui/ArrowLink";
+
+import * as Styles from "./Categories.module.css";
+
+const getCategories = async () => {
+  const response = await fetch(CATEGORIES_URL);
+  return response.json();
+};
+
+const Categories = async () => {
+  const categories = await getCategories();
+  const resultList = categories.results.map((item, index) => (
+    <ArrowLink
+      text={item.list_name}
+      address={`/list/${item.list_name_encoded}`}
+      key={index}
+    >
+      isOuterLink={false}
+      {item.list_name}
+    </ArrowLink>
+  ));
+  return <div className={Styles.container}>{resultList}</div>;
+};
+
+export default Categories;
