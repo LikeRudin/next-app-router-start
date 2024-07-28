e39cc5f8a93b5cadca7223d12089c3782adef29c
Young Jin <likeRudin@gmail.com>
2024-07-28 22:55:16 +0900

    [Page] add /about

---
+++ next-js-start/next-book-shelf/app/about/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "About Project",
+};
+
+const About = () => (
+  <div>
+    <h1>Home Work Page</h1>
+  </div>
+);
+
+export default About;
+
+export const runtime = "edge";
